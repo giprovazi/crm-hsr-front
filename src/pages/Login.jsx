dc4e93fc3e07b629ef07746f41a5cc601b7d52f6
giovanni <giovannirprovazi@gmail.com>
2026-02-19 19:18:02 -0300

    Ajustando responsividade

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,54 +45,61 @@ const Login = () => {
 
     return (
 
-        <div className="h-screen flex">
+        <div className="min-h-screen flex">
             <div className="w-1/2">
                 <img src={FundoLogin} alt="Fundo do login" className="h-full w-full object-cover"/>
             </div>
 
-            <div className="w-1/2 bg-[#F4F4F4] flex flex-col items-center py-32 gap-28 shadow-[-43px_0px_26.5px_0px_rgba(0,0,0,0.25)]">
-                <img src={Logo} alt="Logo" className="w-[8.5rem] " />
-                <form onSubmit={handleSubmit} className="bg-[rgba(0,161,230,0.85)] w-full max-w-sm md:max-w-lg rounded-3xl px-6  pb-10 pt-7 shadow-2xl flex flex-col  ">
+            <div className="w-1/2 bg-[#F4F4F4] flex flex-col gap-10 shadow-[-43px_0px_26.5px_0px_rgba(0,0,0,0.25)]">
 
-                    <h1 className="font-konkhmer text-white text-3xl text-center mb-5 mt-4">Login</h1>
 
-                    <div className="flex flex-col items-start w-full gap-12 px-5">
-                        <div className="w-full">
-                            <p className="font-konkhmer text-white text-[19px] mb-2">Email</p>
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Digite seu email..." className=" placeholder:font-konkhmer placeholder:text-[#b8b8b8c5] text-[16px] w-full py-6 px-3 rounded-lg outline-none border-none appearance-none focus:outline-none focus:ring-0"/>
+                <div className="flex-1 flex flex-col items-center justify-center">
+                    <div className="pb-28 pt-4">
+                        <img src={Logo} alt="Logo" className="w-[10rem] 2xl:w-[11rem]" />
+                    </div> 
+                    <form onSubmit={handleSubmit} className="bg-[rgba(0,161,230,0.85)] w-full max-w-sm md:max-w-xl 2xl:max-w-3xl  rounded-3xl px-6  pb-10 pt-7 shadow-2xl flex flex-col  ">
+
+                        <h1 className="font-konkhmer text-white text-4xl text-center mb-5 mt-4">Login</h1>
+
+                        <div className="flex flex-col items-start w-full gap-12 px-5">
+                            <div className="w-full">
+                                <p className="font-konkhmer text-white text-[24px] mb-2">Email</p>
+                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Digite seu email..." className=" placeholder:font-konkhmer placeholder:text-[#b8b8b8c5] text-[20px] w-full py-6 px-3 rounded-lg outline-none border-none appearance-none focus:outline-none focus:ring-0"/>
+                            </div>
+
+                            <div className="w-full">
+                                <p className="font-konkhmer text-white text-[24px] mb-2">Senha</p>
+                                <input type="password" value={senha} onChange={(e)=> setSenha(e.target.value)} placeholder="Digite sua senha..." className="w-full font-konkhmer placeholder:text-[#b8b8b8c5] text-black text-[18px] py-6 px-3 rounded-lg outline-none border-none appearance-none focus:outline-none focus:ring-0 "/>
+                            </div> 
+
+                        {erro &&
+                                <div className="w-full flex items-start justify-between gap-4 border border-red-500 bg-red-50 rounded-lg px-4 py-3">
+                                    
+                                    <p className="text-red-700 text-sm">
+                                    {erro}
+                                    </p>
+
+                                    <button
+                                    type="button"
+                                    onClick={() => setErro("")}
+                                    className="text-red-700 font-bold hover:text-red-900"
+                                    aria-label="Fechar mensagem de erro"
+                                    >
+                                    ✕
+                                    </button>
+
+                                </div>
+                            }              
                         </div>
 
-                        <div className="w-full">
-                            <p className="font-konkhmer text-white text-[19px] mb-2">Senha</p>
-                            <input type="password" value={senha} onChange={(e)=> setSenha(e.target.value)} placeholder="Digite sua senha..." className="w-full font-konkhmer placeholder:text-[#b8b8b8c5] text-black text-[16px] py-6 px-3 rounded-lg outline-none border-none appearance-none focus:outline-none focus:ring-0 "/>
-                        </div> 
-
-                       {erro &&
-                            <div className="w-full flex items-start justify-between gap-4 border border-red-500 bg-red-50 rounded-lg px-4 py-3">
-                                
-                                <p className="text-red-700 text-sm">
-                                {erro}
-                                </p>
-
-                                <button
-                                type="button"
-                                onClick={() => setErro("")}
-                                className="text-red-700 font-bold hover:text-red-900"
-                                aria-label="Fechar mensagem de erro"
-                                >
-                                ✕
-                                </button>
-
-                            </div>
-                        }              
-                    </div>
-
-                    <div className="flex justify-center mt-8">
-                        <button type="submit" className="px-10 py-3 rounded-xl bg-white text-[rgba(0,161,230,0.85)] shadow-lg font-konkhmer text-lg hover:bg-slate-50">Entrar</button>
-                    </div>
+                        <div className="flex justify-center mt-8">
+                            <button type="submit" className="px-10 py-3 rounded-xl bg-white text-[rgba(0,161,230,0.85)] shadow-lg font-konkhmer text-xl 2xl:text-2xl hover:bg-slate-50">Entrar</button>
+                        </div>
 
 
-                </form>
+                    </form>                    
+                </div>               
+
                 
             </div>
         </div>
